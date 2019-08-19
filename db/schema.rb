ddef5e971033ad363ece4a39e014ea4a97c3c484
Brett <brett.simington@gmail.com>
2019-08-19 22:47:51 +0000

    lesson32: Sections complete

--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,9 @@ ActiveRecord::Schema.define(version: 2019_08_19_221132) do
     t.integer "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "row_order"
     t.index ["course_id"], name: "index_sections_on_course_id"
+    t.index ["row_order"], name: "index_sections_on_row_order"
   end
 
   create_table "users", force: :cascade do |t|
